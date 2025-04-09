e9848ae8b441b48e046057651985d3dd4e450ebd
chrisroselli <croselli@contractornation.com>
2025-04-09 15:45:56 -0400

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,9 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Treehouse Form Builder',
   description: 'Form builder tool for Treehouse Production team',
+  icons: {
+    icon: 'https://cdn.treehouseinternetgroup.com/cms_images/687/th-mkg-favicon.ico',
+  },
   generator: 'v0.dev',
 };
 
